59e95dd93e9035feac6fafd62eeb0401e906af34
Piotr Lawniczek <piotr.lawniczek93@gmail.com>
2019-02-27 18:40:41 +0100

    initial commit

---
+++ src/main/java/p/lawniczek/LiveScoreBackendApplication.java
@@ -0,0 +1,13 @@
+package p.lawniczek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveScoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveScoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/p/lawniczek/LiveScoreBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package p.lawniczek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiveScoreBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
