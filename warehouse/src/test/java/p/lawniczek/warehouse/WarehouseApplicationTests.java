89e0338052a556b838766c56ce7b95fd537b4776
Piotr Lawniczek <piotr.lawniczek93@gmail.com>
2020-08-12 10:29:32 +0200

    initialize warehouse microservice

---
+++ warehouse/src/test/java/p/lawniczek/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package p.lawniczek.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
